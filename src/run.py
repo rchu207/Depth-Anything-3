e26543503771a3f6182d0c4de50924c25c2c21eb
Rafael Chu <rchu@pixelworks.com>
2025-12-19 21:34:31 +0800

    Update batch run script.

--- src/run.py
+++ src/run.py
@@ -8,6 +8,7 @@ import argparse
 import matplotlib
 from depth_anything_3.api import DepthAnything3
 from depth_anything_3.specs import Prediction
+from depth_anything_3.utils.logger import logger
 
 
 def export_to_mini_npz(
@@ -103,7 +104,7 @@ if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = DepthAnything3.from_pretrained(args.load_from)
     model = model.to(device=device).eval()
-    print(f"Model {args.load_from} loaded on {device}")
+    logger.info(f"Model {args.load_from} loaded on {device}")
 
     if os.path.isfile(args.img_path):
         filenames = [args.img_path]
@@ -112,33 +113,37 @@ if __name__ == '__main__':
     os.makedirs(args.outdir, exist_ok=True)
 
     # Run inference on images
+    summary = []
     for k, filename in enumerate(filenames):
-        # print(f'Progress {k+1}/{len(filenames)}: {filename}')
+        if not os.path.isfile(filename):
+            continue
+        logger.info(f'Progress {k+1}/{len(filenames)}: {filename}')
+
         images = [filename, ]  # List of image paths, PIL Images, or numpy arrays
-        # output_path = os.path.join(args.outdir, os.path.splitext(os.path.basename(filename))[0])
-        # prediction = model.inference(
-        #     images,
-        #     export_dir=output_path,
-        #     export_format="mini_npz-depth_vis"
-        # )
         prediction = model.inference(
             images,
         )
 
         # Access results
-        print(prediction.processed_images.shape) # Processed images : [N, H, W, 3] uint8   array
-        print(prediction.depth.shape)        # Depth maps: [N, H, W] float32
+        logger.info(prediction.processed_images.shape) # Processed images : [N, H, W, 3] uint8   array
+        logger.info(prediction.depth.shape)        # Depth maps: [N, H, W] float32
         if prediction.conf is not None:
-            print(prediction.conf.shape)         # Confidence maps: [N, H, W] float32
+            logger.info(prediction.conf.shape)         # Confidence maps: [N, H, W] float32
         if prediction.extrinsics is not None:
-            print(prediction.extrinsics.shape)   # Camera poses (w2c): [N, 3, 4] float32
+            logger.info(prediction.extrinsics.shape)   # Camera poses (w2c): [N, 3, 4] float32
         if prediction.intrinsics is not None:
-            print(prediction.intrinsics.shape)   # Camera intrinsics: [N, 3, 3] float32
-            print(prediction.intrinsics)
+            logger.info(prediction.intrinsics.shape)   # Camera intrinsics: [N, 3, 3] float32
+            logger.info(prediction.intrinsics)
 
         # inferred depth map [H,W].
         infer_depth = prediction.depth.squeeze(0)
-        print(filename, infer_depth.shape[0], infer_depth.shape[1], infer_depth.min(), infer_depth.max())
+        if prediction.intrinsics is not None:
+            fx = prediction.intrinsics[0][0][0]
+            fy = prediction.intrinsics[0][1][1]
+        else:
+            fx = 0.0
+            fy = 0.0
+        summary.append(f'{filename},{infer_depth.shape[0]},{infer_depth.shape[1]},{infer_depth.min():.8},{infer_depth.max():.8},{fx:.8},{fy:.8}')
 
         # store inferred depth map to npz
         export_to_mini_npz(infer_depth, prediction, args.outdir, filename)
@@ -149,3 +154,7 @@ if __name__ == '__main__':
         # store inferred depth map to colormap
         scaled_image = prediction.processed_images.squeeze(0)
         export_to_depth_vis(scaled_image, infer_depth, args.outdir, filename)
+
+    print('----------Summary----------')
+    for msg in summary:
+        print(msg)
